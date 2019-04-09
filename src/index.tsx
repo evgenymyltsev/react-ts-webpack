5faee30e581bf813bcce96f93a13283da2c554ee
Evgeny Myltsev <myltsev.ea@gmail.com>
2019-04-09 14:24:30 +0300

    simple ts react todo with useState

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,64 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import ReactDOM from 'react-dom';
 
-export default function App(): JSX.Element {
-    const sum = (a:number, b:number):number => a + b
-    return (
-        <h1>
-          Hello {sum(5, 15)}
-        </h1>
-    )
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+    text: string
+    complete: boolean
 }
 
+export default function App(): JSX.Element {
+    const [value, setValue] = useState<string>('');
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    const handleSubmit = (e:FormElem):void => {
+        e.preventDefault();
+        addTodo(value);
+        setValue('');
+    }
+
+    const addTodo = (text:string):void => {
+        const newTodos:ITodo[] = [...todos, { text, complete: false }];
+        setTodos(newTodos);
+    }
+    
+    const completeTodo = (index:number):void => {
+        const newTodos:ITodo[] = [...todos];
+        newTodos[index].complete = !newTodos[index].complete;
+        setTodos(newTodos);
+    }
+
+    const removeTodo = (index:number):void => {
+        const newTodos:ITodo[] = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+
+    return (
+        <>
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={value} onChange={e => setValue(e.target.value)} required />
+                <button type='submit'>Add Todo</button>
+            </form>
+            <section>
+                {todos.map((todo:ITodo, index) =>
+                    <div key={index} style={{ textDecoration: todo.complete ? 'line-through' : ''}}>
+                        {todo.text}
+                        <button type='button' onClick={() => completeTodo(index)}>
+                            {' '}{todo.complete ? 'Incomplete' : 'Complete'}{' '}
+                        </button>
+                        <button type='button' onClick={() => removeTodo(index)}>
+                            &times;
+                        </button>
+                    </div>
+                )}
+            </section>
+        </>
+    )
+}
+  
 const root = document.getElementById('app-root');
 
 ReactDOM.render(<App />, root);
